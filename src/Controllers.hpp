cdb0e0019088abfc3e4b795a0d31fc71e7a73f7f
Thomas Birbacher <thomas@birbacher.com>
2024-10-24 10:30:18 +0200

    Refactor Controllers class to be header-only

--- src/Controllers.hpp
+++ src/Controllers.hpp
@@ -1,10 +1,24 @@
 class PIController {
  public:
-  PIController();
-  void setPI(float p, float i);
-  void reset_state();
-  void reset_all();
-  float step(float input);
+  PIController() {
+    p = 0;
+    i = 0;
+    error_integral = 0;
+  }
+  void setPI(float p, float i) {
+    this->p = p;
+    this->i = i;
+  }
+  void reset_state() { error_integral = 0; }
+  void reset_all() {
+    error_integral = 0;
+    p = 0;
+    i = 0;
+  }
+  float step(float input) {
+    error_integral += input;
+    return p * input + i * error_integral;
+  }
 
  private:
   float p;
